c103af6c95b2b198bd18fcc3f3c94e3e2f612510
erikatonosu <e.4.se.sa@gmail.com>
2016-01-31 15:37:53 +0900

    緯度経度

--- js/touch.js
+++ js/touch.js
@@ -1,6 +1,24 @@
 // DOMロード
 $(function(){
 
+// 緯度
+var lon;
+var lat;
+
+// 経度
+
+    //GPS
+    navigator.geolocation.getCurrentPosition(function(evt){
+        // 緯度
+        lon = evt.coords.longitude;
+        // 経度
+        lat = evt.coords.latitude;
+    },
+    function(error){
+        console.log('GPS取れなかったよー');
+    });
+
+
     // タッチイベント
     $('#ball').on('touchstart', function(){
         $('#touchResult li').eq(0).text('touchstartしたよ');
@@ -12,6 +30,10 @@ $(function(){
 
     $('#ball').on('touchend', function(){
         $('#touchResult li').eq(2).text('touchendしたよ');
+
+        $('#geo li').eq(0).text('緯度: ' + lon);
+        $('#geo li').eq(1).text('経度: ' + lat);
+
     });
 
 
