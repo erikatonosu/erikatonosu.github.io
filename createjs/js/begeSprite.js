3262b92e74b91e948ec573eeb88c8f34b9dd5198
erikatonosu <e.4.se.sa@gmail.com>
2016-02-07 16:39:10 +0900

    up

--- createjs/js/begeSprite.js
+++ createjs/js/begeSprite.js
@@ -10,7 +10,7 @@ var vitaminCSprite = {
 var ironSprite = {
     images:['img/iron.png'],
     frames:{width:160, height:160},
-    animations{
+    animations:{
         stand:34,
         walk:[0,33]
     }
