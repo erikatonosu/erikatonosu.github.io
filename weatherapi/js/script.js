8e673ea49edddb810da6beda7a12aa7e9ef78956
erikatonosu <e.4.se.sa@gmail.com>
2016-02-07 11:23:26 +0900

    weather api

---
+++ weatherapi/js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+    //GPS
+    navigator.geolocation.getCurrentPosition(function(pos){
+        var lat = pos.coords.latitude;
+        var lon = pos.coords.longitude;
+
+        console.log(lat, lon);
+    });
+});
