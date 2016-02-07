c41063f51a19a63518ffc709ea473951dd147c28
erikatonosu <e.4.se.sa@gmail.com>
2016-02-07 11:42:06 +0900

    weather api check

--- weatherapi/js/script.js
+++ weatherapi/js/script.js
@@ -5,5 +5,23 @@ $(function(){
         var lon = pos.coords.longitude;
 
         console.log(lat, lon);
+
+        // 定数 = 値が変わらない設定
+        const apikey = '7313e1ec3373e1e5ff3e69e878f40126';
+        // openweathermapのお天気情報のURL
+        var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon +'&appid=' + apikey;
+
+
+        $.ajax({
+            url:url,
+            dataType:'json'
+        }).done(function(data){
+
+        }).fail(function(){
+            console.log('ajax error!!!');
+        });
+
+
+
     });
 });
