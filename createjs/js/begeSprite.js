977e8a88de90f18209b1cdc0d7fbbf0c1f9a5696
erikatonosu <e.4.se.sa@gmail.com>
2016-02-07 16:12:34 +0900

    vitamin

--- createjs/js/begeSprite.js
+++ createjs/js/begeSprite.js
@@ -1,8 +1,8 @@
 var vitaminCSprite = {
-    images:'img/vitaminC.png',
+    images:['img/vitaminC.png'],
     frames:{width:160, height:160},
     animations:{
-        stand:34;
+        stand:34,
         walk:[0,33]
     }
 }
